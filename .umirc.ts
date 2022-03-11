320db921dc27f043467b93f56044e36a790ce4f1
YJKZHUZHU <18470186610@163.com>
2022-03-11 16:30:04 +0800

    部署

--- .umirc.ts
+++ .umirc.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'dumi';
 import { navs, menus } from './router';
 
 export default defineConfig({
-  title: '尛豆芽',
+  title: '尛豆芽hahhaha',
   mode: 'site',
   favicon: '/favicon.ico',
   logo: '/logo.jpg',
